c5a166c1faa8ba48404bce96ac6eb92d71458286
Jignesh Raiyani <jignesh.raiyani1@gmail.com>
2023-12-21 13:43:58 +0530

    Training module integration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage01_data_ingestion import DataIngestionPipeline
 from src.cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
     
@@ -18,6 +19,18 @@ STAGE_NAME2 = "Prepare Base Model Stage"
 try:
     logger.info(f"stage   {STAGE_NAME}  started")
     obj = PrepareBaseModelPipeline()
+    obj.main()
+    logger.info(f"stage   {STAGE_NAME}  completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME3 = "Training Stage"
+    
+try:
+    logger.info(f"stage   {STAGE_NAME}  started")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f"stage   {STAGE_NAME}  completed")
 except Exception as e:
